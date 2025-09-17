c0c28cde297499445aeb996effff051377541ac2
Tushar Kalbhande <tusharkalbhande1805@gmail.com>
2025-09-17 11:44:57 +0530

    Fix: Auth issue fix

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -2,13 +2,15 @@
 import 'package:flutter_project_template/core/localization/language_cubit.dart';
 import 'package:flutter_project_template/core/resources/app_storage.dart';
 import 'package:flutter_project_template/core/theme/theme_cubit.dart';
+import 'package:flutter_project_template/presentation/auth/cubit/auth_cubit.dart';
+import 'package:flutter_project_template/presentation/auth/repo/auth_repo.dart';
 import 'package:get_it/get_it.dart';
 final sl = GetIt.I;
 
 void setupServiceLocator() async {
   //* Repositories
   sl.registerFactory<SecureStorage>(SecureStorage.new);
-  // sl.registerFactory<AuthRepository>(AuthRepository.new);
+  sl.registerFactory<AuthRepository>(AuthRepository.new);
 
   //* Cubits
   sl.registerLazySingleton<ThemeCubit>(
@@ -17,8 +19,8 @@ void setupServiceLocator() async {
   sl.registerLazySingleton<LanguageCubit>(
     LanguageCubit.new,
   );
-  // sl.registerSingleton<AuthCubit>(
-  //   AuthCubit(sl<AuthRepository>()),
-  // );
+  sl.registerSingleton<AuthCubit>(
+    AuthCubit(sl<AuthRepository>()),
+  );
   
 }
